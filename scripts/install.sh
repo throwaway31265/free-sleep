b6646758032b1b62682439a08306c291de38afe2
archelnokov <71182812+archelnokov@users.noreply.github.com>
2025-02-14 03:37:20 +0100

    rm the old repo dir in install.sh
    Without this I was not able to use install.sh to update from the old version.

--- scripts/install.sh
+++ scripts/install.sh
@@ -22,6 +22,7 @@ rm "$ZIP_FILE"
 
 # Step 3: Move files to the installation directory
 echo "Setting up the installation directory..."
+rm -rf "$REPO_DIR"
 mv free-sleep-main "$REPO_DIR"
 chown -R dac:dac "$REPO_DIR"
 
