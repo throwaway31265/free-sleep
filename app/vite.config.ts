25ed9bf1674173185d0eb2ac232ed07abc03f595
Matt Gates <Geczy@users.noreply.github.com>
2025-08-15 08:25:12 -0500

    fix: update Vite configuration and dependencies
    - Simplified Vite commands in package.json
    - Updated server port in Vite config to 3000
    - Refactored baseURL logic in API module for clarity
    - Added ts-essentials as a new dependency

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -4,41 +4,39 @@ import { tanstackRouter } from '@tanstack/router-plugin/vite'
 import { resolve } from 'node:path'
 
 // https://vitejs.dev/config/
-export default defineConfig({
-  plugins: [
-    tanstackRouter({ autoCodeSplitting: false }),
-    viteReact(),
-  ],
-  server: {
-    host: '0.0.0.0', // This makes the server accessible to other devices on the network
-    port: 5173, // Optional: specify a port if you want something other than the default
-  },
-  build: {
-    sourcemap: process.env.NODE_ENV === 'development',
-    outDir: '../server/public/',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'index.js', // Set the name for the JS entry file
-        chunkFileNames: '[name]-[hash].js', // Names for dynamic imports
-        assetFileNames: ({ name }) => {
-          if (name?.endsWith('.css')) {
-            return 'index.css';
-          }
-          return '[name]-[hash].[ext]';
+export default defineConfig(() => {
+  return {
+    plugins: [
+      tanstackRouter({ autoCodeSplitting: false }),
+      viteReact(),
+    ],
+    server: {
+      host: '0.0.0.0', // This makes the server accessible to other devices on the network
+      port: 3000,
+    },
+    build: {
+      sourcemap: process.env.NODE_ENV === 'development',
+      outDir: '../server/public/',
+      rollupOptions: {
+        output: {
+          entryFileNames: 'index.js', // Set the name for the JS entry file
+          chunkFileNames: '[name]-[hash].js', // Names for dynamic imports
+          assetFileNames: ({ name }) => {
+            if (name?.endsWith('.css')) {
+              return 'index.css';
+            }
+            return '[name]-[hash].[ext]';
+          },
         },
       },
     },
-  },
-  test: {
-    globals: true,
-    environment: 'jsdom',
-  },
-  resolve: {
-    alias: {
-      '@': resolve(__dirname, './src'),
-      '@api': resolve(__dirname, './src/api'),
-      '@state': resolve(__dirname, './src/state'),
-      '@components': resolve(__dirname, './src/components'),
+    resolve: {
+      alias: {
+        '@': resolve(__dirname, './src'),
+        '@api': resolve(__dirname, './src/api'),
+        '@state': resolve(__dirname, './src/state'),
+        '@components': resolve(__dirname, './src/components'),
+      },
     },
-  },
+  }
 })
