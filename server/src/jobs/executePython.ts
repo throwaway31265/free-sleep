bfe71517dc1071b608c8e1de3ff5f6d2c1a2083d
Matt Gates <mgates@hubspot.com>
2025-08-23 07:12:24 -0500

    revert

--- server/src/jobs/executePython.ts
+++ server/src/jobs/executePython.ts
@@ -24,10 +24,7 @@ export const executePythonScript = ({
     const command = `sudo -u dac ${pythonExecutable} -B ${script} ${args.join(' ')}`;
     logger.info(`Executing: ${command}`);
 
-    exec(command, {
-      env: { ...process.env },
-      cwd: '/home/dac/free-sleep/biometrics'
-    }, (error, stdout, stderr) => {
+    exec(command, { env: { ...process.env } }, (error, stdout, stderr) => {
       if (error) {
         logger.error(`Execution error: ${error.message}`);
         reject(error);
