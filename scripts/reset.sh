b38e0acdad15410d4968825e9813e2b32ff97273
David <noahmeier82@proton.me>
2025-10-21 18:02:04 -0700

    Add script to fix free-sleep-data directory. Beta branch broke schedules for some people

---
+++ scripts/reset.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+echo "WARNING: This will permanently delete all Free Sleep data located at /persistent/free-sleep-data/"
+read -p "Are you sure you want to continue? (y/N): " confirm
+
+if [[ "$confirm" =~ ^[Yy]$ ]]; then
+    echo "Deleting Free Sleep data..."
+    rm -rf /persistent/free-sleep-data/test/
+    echo "Data deleted."
+else
+    echo "Operation cancelled."
+fi
