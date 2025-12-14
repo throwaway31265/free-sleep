da4a35cb8b8217c28603b3f7a6f363f5ea816b24
Jonathan Ng <jon@jonathanng.com>
2025-12-13 18:18:00 -0800

    refactor(Layout): clean up layout

--- app/src/components/Layout.tsx
+++ app/src/components/Layout.tsx
@@ -1,26 +1,32 @@
-import { Outlet } from 'react-router-dom';
-import Navbar from './Navbar';
+
 import Box from '@mui/material/Box';
+import { useAppStore } from '@state/appStore';
+import { Outlet } from 'react-router-dom';
+import { Navbar } from './Navbar';
+import { LoadingBar } from './ui/LoadingBar';
 
+/**
+ * The `Layout` component serves as the primary layout wrapper for the application.
+ * It includes global elements such as the `Navbar` and `LoadingBar`, and renders
+ * the current route using the `Outlet` component.
+ */
+export const Layout = () => {
+  const { isUpdating } = useAppStore();
 
-export default function Layout() {
   return (
     <Box
       id="Layout"
-      sx={ {
+      sx={{
+        padding: 1,
         display: 'flex',
-        flexDirection: 'column',
-        flexGrow: 1,
-        alignItems: 'center',
-        gap: 2,
-        // padding: 0,
-        margin: 0,
         justifyContent: 'center',
-      } }
+      }}
     >
-      { /* Renders current route */ }
-      <Outlet/>
-      <Navbar/>
+      <LoadingBar isUpdating={isUpdating} />
+
+      { /* Renders current route */}
+      <Outlet />
+      <Navbar />
     </Box>
   );
 }
