12cf8423c2e5b601b26be30690ad5bc6921ad1f3
Jonathan Ng <jon@jonathanng.com>
2025-12-13 18:18:14 -0800

    refactor(main.tsx): update imports

--- app/src/main.tsx
+++ app/src/main.tsx
@@ -1,46 +1,30 @@
+import * as Sentry from '@sentry/react';
 import { initSentry } from './sentry.tsx';
 initSentry();
-import * as Sentry from '@sentry/react';
 
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
-import { createRoot } from 'react-dom/client';
 import { CssBaseline } from '@mui/material';
+import { ThemeProvider } from '@mui/material/styles';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { StrictMode } from 'react';
-import { ThemeProvider, createTheme } from '@mui/material/styles';
+import { createRoot } from 'react-dom/client';
+import { BrowserRouter, Route, Routes } from 'react-router-dom';
 
-import ControlTempPage from './pages/ControlTempPage/ControlTempPage';
-import SettingsPage from './pages/SettingsPage/SettingsPage';
-import Layout from './components/Layout';
-import { AppStoreProvider } from '@state/appStore.tsx';
-import SchedulePage from './pages/SchedulePage/SchedulePage.tsx';
-import ErrorBoundary from './components/ErrorBoundary.tsx';
 import { GlobalStyles } from '@mui/material';
-import SleepPage from './pages/DataPage/SleepPage/SleepPage.tsx';
-import DataPage from './pages/DataPage/DataPage.tsx';
-import VitalsPage from './pages/DataPage/VitalsPage/VitalsPage.tsx';
-import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
 import { AdapterMoment } from '@mui/x-date-pickers/AdapterMoment';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { AppStoreProvider } from '@state/appStore.tsx';
+import ErrorBoundary from './components/ErrorBoundary.tsx';
+import { Layout } from './components/Layout';
+import ControlTempPage from './pages/ControlTempPage/ControlTempPage';
+import DataPage from './pages/DataPage/DataPage.tsx';
 import LogsPage from './pages/DataPage/LogsPage/LogsPage.tsx';
+import SleepPage from './pages/DataPage/SleepPage/SleepPage.tsx';
+import VitalsPage from './pages/DataPage/VitalsPage/VitalsPage.tsx';
+import SchedulePage from './pages/SchedulePage/SchedulePage.tsx';
+import SettingsPage from './pages/SettingsPage/SettingsPage';
 import StatusPage from './pages/StatusPage/StatusPage.tsx';
-
-const darkTheme = createTheme({
-  palette: {
-    mode: 'dark',
-    background: {
-      default: '#010101'
-    },
-    grey: {
-      100: '#e8eaed',
-      300: '#a6adbe',
-      400: '#88878c',
-      500: '#606060',
-      700: '#272727',
-      800: '#262626',
-      900: '#242424',
-    }
-  },
-});
+import { darkTheme } from './theme/theme';
+import './styles/animations.css';
 
 const queryClient = new QueryClient({
   defaultOptions: {
@@ -52,40 +36,37 @@ const queryClient = new QueryClient({
 
 const SentryRoutes = Sentry.withSentryReactRouterV7Routing(Routes);
 
-
 const App = () => {
   return (
-
-    <QueryClientProvider client={ queryClient }>
-      <ThemeProvider theme={ darkTheme }>
-        <LocalizationProvider dateAdapter={ AdapterMoment }>
-
+    <QueryClientProvider client={queryClient}>
+      <ThemeProvider theme={darkTheme}>
+        <LocalizationProvider dateAdapter={AdapterMoment}>
           <AppStoreProvider>
-            <CssBaseline/>
+            <CssBaseline />
             <GlobalStyles
-              styles={ {
+              styles={{
                 'html, body': {
                   overscrollBehavior: 'none', // Prevent rubber-banding
                 },
-              } }
+              }}
             />
             <BrowserRouter basename="/">
               <SentryRoutes>
-                <Route path="/" element={ <Layout/> }>
-                  <Route index element={ <ControlTempPage/> }/>
-                  <Route path="temperature" element={ <ControlTempPage/> }/>
-                  <Route path="left" element={ <ControlTempPage/> }/>
-                  <Route path="right" element={ <ControlTempPage/> }/>
-                  <Route path="status" element={ <StatusPage /> } />
+                <Route path="/" element={<Layout />}>
+                  <Route index element={<ControlTempPage />} />
+                  <Route path="temperature" element={<ControlTempPage />} />
+                  <Route path="left" element={<ControlTempPage />} />
+                  <Route path="right" element={<ControlTempPage />} />
+                  <Route path="status" element={<StatusPage />} />
 
-                  <Route path="data" element={ <DataPage /> }>
-                    <Route path="sleep" element={ <SleepPage/> }/>
-                    <Route path="logs" element={ <LogsPage/> }/>
-                    <Route path="vitals" element={ <VitalsPage/> }/>
+                  <Route path="data" element={<DataPage />}>
+                    <Route path="sleep" element={<SleepPage />} />
+                    <Route path="logs" element={<LogsPage />} />
+                    <Route path="vitals" element={<VitalsPage />} />
                   </Route>
 
-                  <Route path="settings" element={ <SettingsPage/> }/>
-                  <Route path="schedules" element={ <SchedulePage/> }/>
+                  <Route path="settings" element={<SettingsPage />} />
+                  <Route path="schedules" element={<SchedulePage />} />
                 </Route>
               </SentryRoutes>
             </BrowserRouter>
