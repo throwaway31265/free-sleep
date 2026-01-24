cf222d3c200464b36eae635598b17afc27b472ae
Oliver Nemec <28827159+onemec@users.noreply.github.com>
2026-01-24 17:55:05 -0500

    Update git info

--- app/src/components/VitalsLineChart.tsx
+++ app/src/components/VitalsLineChart.tsx
@@ -42,7 +42,7 @@ const Banner = ({ metric }: BannerProps) => {
       text: <Typography>Heart rate data has been validated with six participants, and accuracy may be limited.
         You can help improve future accuracy by contributing your own data for validation or
         by experimenting and improving the algorithm yourself.
-        See the <Link href='https://github.com/throwaway31265/free-sleep?tab=readme-ov-file#biometrics-'>documentation</Link>
+        See the <Link href='https://github.com/onemec/free-sleep?tab=readme-ov-file#biometrics-'>documentation</Link>
         &nbsp;for details on current measurement accuracy.
       </Typography>,
     },
